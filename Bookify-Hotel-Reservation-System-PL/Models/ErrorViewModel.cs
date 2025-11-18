8726401ee4aa512c69cdb94c2f332bd229dfcd82
mohamed ali <mohamedalim559@gmail.com>
2025-11-18 19:29:49 +0200

    Add project files.

---
+++ Bookify-Hotel-Reservation-System-PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookify_Hotel_Reservation_System_PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
