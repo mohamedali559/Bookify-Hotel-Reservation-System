810e6436327b69976b9af5762c2f720e45ed16be
ammar <ammarabdelateif23@gmail.com>
2025-11-29 08:28:40 +0200

    Refactor RoomController and Index view for dynamic data
    Refactored `RoomController` to use `GetAllWithAmenitiesAndRoomType`
    for fetching detailed room data. Introduced `RoomDetailsViewModel`
    to encapsulate room details, including amenities and room type
    information. Updated `Index.cshtml` to dynamically render room
    cards using the new view model. Replaced static HTML with a
    `foreach` loop for dynamic data binding. Added Bootstrap styling
    for improved UI. Enhanced "View Details" button with dynamic
    routing using `asp-route-id`.

--- Bookify-Hotel-Reservation-System-PL/Models/RoomDetailsViewModel.cs
+++ Bookify-Hotel-Reservation-System-PL/Models/RoomDetailsViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Bookify_Hotel_Reservation_System_PL.Models
+﻿using Bookify_Hotel_Reservation_System__DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify_Hotel_Reservation_System_PL.Models
 {
     public class RoomDetailsViewModel
     {
+        public int RoomId { get; set; }
+        public int Floor { get; set; }
+        public string ImageUrl { get; set; }
+        public string RoomTypeName { get; set; }
+        public string RoomDescription { get; set; }
+        public decimal Area { get; set; }
+        public int Guests { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public List<Amenity> Amenities { get; set; }
     }
+
 }
